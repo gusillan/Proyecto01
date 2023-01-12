bb470feaaa03dc9261f5b50f83f3738797713621
PC_Torre\Gustavo <gusillan@gmail.com>
2023-01-12 23:24:19 +0100

    Integracion entidad con BBDD

---
+++ src/main/java/com/pacoillan/main/Principal.java
@@ -0,0 +1,25 @@
+package com.pacoillan.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.pacoillan.bo.Entidad;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("unidadPersistencia");
+		EntityManager em = emf.createEntityManager();
+		Entidad entidad = em.find(Entidad.class,1);
+		
+		System.out.println(entidad.getNombre());
+		
+		
+		
+		
+
+	}
+
+}
